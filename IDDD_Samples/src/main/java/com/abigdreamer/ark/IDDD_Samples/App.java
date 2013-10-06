0fb6bb9a797bf2ed4a6d486c0e48fc8df00df1d2
darkness <darkness.itachi@gmail.com>
2013-10-06 17:25:52 +0800

    init

---
+++ IDDD_Samples/src/main/java/com/abigdreamer/ark/IDDD_Samples/App.java
@@ -0,0 +1,13 @@
+package com.abigdreamer.ark.IDDD_Samples;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
